f70bf3a25b7e404b9f3060a106f984b153d7ebd7
Apple Ng <appleng113@gmail.com>
2017-06-18 23:58:09 +0800

    Test on react-router

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,45 @@
-// 這邊使用 HtmlWebpackPlugin，將 bundle 好的 <script> 插入到 body。${__dirname} 為 ES6 語法對應到 __dirname  
+// 讓你可以動態插入 bundle 好的 .js 檔到 .index.html
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
-  template: `${__dirname}/app/index.html`,
+  template: `${__dirname}/src/index.html`,
   filename: 'index.html',
   inject: 'body',
 });
 
+// entry 為進入點，output 為進行完 eslint、babel loader 轉譯後的檔案位置
 module.exports = {
-  // 檔案起始點從 entry 進入，因為是陣列所以也可以是多個檔案
   entry: [
-    './app/index.js',
+    './src/index.js',
   ],
-  // output 是放入產生出來的結果的相關參數
   output: {
     path: `${__dirname}/dist`,
     filename: 'index_bundle.js',
   },
   module: {
-  	// loaders 則是放欲使用的 loaders，在這邊是使用 babel-loader 將所有 .js（這邊用到正則式）相關檔案（排除了 npm 安裝的套件位置 node_modules）轉譯成瀏覽器可以閱讀的 JavaScript。preset 則是使用的 babel 轉譯規則，這邊使用 react、es2015。若是已經單獨使用 .babelrc 作為 presets 設定的話，則可以省略 query
-    loaders: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['es2015', 'react'],
+    rules: [
+        {
+          test: /\.jsx$|\.js$/,
+          loader: 'eslint-loader',
+          include: `${__dirname}/src`,
+          exclude: /bundle\.js$/,
+          enforce: "pre"
         },
-      },
+        {
+           test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            query: {
+              presets: ['es2015', 'react'],
+            },
+        },
     ],
+  
   },
-  // devServer 則是 webpack-dev-server 設定
+  // 啟動開發測試用 server 設定（不能用在 production）
   devServer: {
     inline: true,
     port: 8008,
   },
-  // plugins 放置所使用的外掛
   plugins: [HTMLWebpackPluginConfig],
 };
